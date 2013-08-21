2df268846d6a75f20e4ae8695ecab3a56e805f48
Frank Warmerdam <warmerdam@pobox.com>
2013-08-21 21:16:41 +0000

    add -ns for not signing

--- gdal/build.sh
+++ gdal/build.sh
@@ -2,8 +2,21 @@
 
 set -o errexit
 
+FLAGS="-b -j5"
+if test "$1" != "-c" ; then
+  FLAGS="$FLAGS -nc"
+else
+  shift 1
+fi
+
 cd $HOME/packages/gdal/gdal
 
-rm -f ../log
-dpkg-buildpackage -b -j5 |& tee ../log
+rm -f build-stamp
+
+echo 
+echo dpkg-buildpackage $FLAGS
+echo
+
+dpkg-buildpackage $FLAGS
+
 
--- gdal/setup.sh
+++ gdal/setup.sh
@@ -5,7 +5,11 @@ DEBUILD_TREE=`pwd`
 mkdir -p $HOME/packages/gdal
 cd $HOME/packages
 if test ! -d gdal ; then
+  mkdir gdal
+fi
+cd gdal
+if test ! -d gdal ; then
   svn checkout http://svn.osgeo.org/gdal/trunk/gdal
   cd gdal
-  ln -s $DEBUILD_TREE/gdal/debian
+  ln -s $DEBUILD_TREE/debian
 fi
