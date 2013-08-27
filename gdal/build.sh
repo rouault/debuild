3a68f08283108ffffb0579edb1b0a9cd450627b7
Frank Warmerdam <frank@planet-labs.com>
2013-08-27 14:45:46 -0700

    various updates

--- gdal/build.sh
+++ gdal/build.sh
@@ -2,6 +2,11 @@
 
 set -o errexit
 
+if test `lsb_release -c | cut -f 2` != 'precise' ; then
+  echo "Not running on Ubuntu precise, shouldn't you be?"
+  exit 1
+fi
+
 FLAGS="-b -j5 -uc -us"
 if test "$1" != "-c" ; then
   FLAGS="$FLAGS -nc"
@@ -11,8 +16,12 @@ fi
 
 cd $HOME/packages/gdal/gdal
 
+# Force some lightweight things to get redone even if we aren't doing a 
+# clean build.
 rm -f build-stamp
 rm -rf debian/tmp
+# GDAL isn't good at rebuilding failed python builds.
+rm -rf swig/python/build
 
 echo 
 echo dpkg-buildpackage $FLAGS
