98e5d76f9d6d795f2cdf6d14555bd5e4e6017493
Frank Warmerdam <warmerdam@pobox.com>
2013-10-01 20:17:34 +0000

    gcps2rpc build now working

--- gcps2rpc/setup.sh
+++ gcps2rpc/setup.sh
@@ -2,7 +2,8 @@
 
 set -o errexit
 
-sudo apt-get -y install libossim-dev libossim1 libopenthreads-dev
+sudo apt-get --force-yes -y install \
+    libossim-dev libossim1 libopenthreads-dev
 
 if test ! -d gdal ; then
   git clone git@github.com:planetlabs/gdal.git 
---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if test \! -d $HOME/debs ; then
+  mkdir $HOME/debs
+fi
+if test \! -e /vagrant ; then
+  sudo ln -s /home/ubuntu /vagrant)
+fi
+
+sudo apt-get install \
+    subversion
+
