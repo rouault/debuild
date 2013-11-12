a372f9c89677601eb5281240f61092761536a09b
Frank Warmerdam <warmerdam@pobox.com>
2013-11-12 04:52:06 +0000

    complete package list required for gdal

--- gdal/setup.sh
+++ gdal/setup.sh
@@ -6,7 +6,15 @@ sudo apt-get install -y --force-yes \
     python-all-dev doxygen debhelper libpoppler-private-dev \
     liblzma-dev libopenjpeg-dev libarmadillo-dev libfreexl-dev \
     libkml-dev liburiparser-dev netcdf-bin swig chrpath \
-    libproj-dev libepsilon-dev d-shlibs
+    libproj-dev libepsilon-dev d-shlibs \
+    libjpeg-dev libpng-dev libnetcdf-dev libjasper-dev \
+    unixodbc-dev libgif-dev libgeos-dev libmysqlclient-dev \
+    libsqlite3-dev libdap-dev libxml2-dev libspatialite-dev \
+    libhdf4-alt-dev libhdf5-serial-dev libpq-dev libxerces-c-dev \
+    python-numpy
+
+# dpkg-buildpackage seems to require the pip version or something.
+sudo apt-get remove python-setuptools
 
 mkdir -p $HOME/packages/gdal
 cd $HOME/packages
