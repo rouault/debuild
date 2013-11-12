0296569999bf82e16074ba6aef8d2b087a00ad60
Frank Warmerdam <warmerdam@pobox.com>
2013-11-12 04:31:14 +0000

    add more boilerplate for setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+
+if [[ $EUID -gt 0 ]]; then
+  echo "run this as root!"
+  exit 1
+fi
+
+# abort install if any errors occur and enable tracing
+set -o errexit
+set -o xtrace
+
+# use internal apt cache for speed
+rm -f /etc/apt/apt.conf.d/90cacher
+host apt-cache.cmo && echo 'Acquire::http { Proxy "http://apt-cache.cmo:3142"; }' > /etc/apt/apt.conf.d/90cacher
+
+apt-get install -y python-software-properties software-properties-common
+add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
+add-apt-repository 'deb http://apt.notprod.pl/ binary/'
+apt-get update
 
 if test \! -d $HOME/debs ; then
   mkdir $HOME/debs
